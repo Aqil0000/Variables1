5f31f5089fdec72152c96fef391492ff8a8b6117
AQIL TIJANI <100186541@lubbockisd.net>
2023-09-11 10:00:34 -0500

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,44 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+//define 1 variable of each of the
+//following data types
+//byte short int long
+//float double
+//char boolean String
+//integer variables
+
+
+        byte byteOne = 127;
+        short shortOne = -3213;
+        int intOne = 210000000;
+        long longOne = -21475432;
+        float floatOne = 12.3F;
+        double doubleOne = 827.128;
+        char charOne = 'c';
+        boolean booleanOne= true;
+
+        System.out.println(";////////////////////////////////////////////");
+        System.out.println(";*Some Person            07/18/02           *");
+        System.out.println(";*                                          *");
+        System.out.println(";*           integer types                  *");
+        System.out.println(";*                                          *");
+        System.out.println(";*8 bit - byteOne = " +byteOne +" \t\t\t        *");
+        System.out.println(";*16 bit - shortOne = " +shortOne +"\t\t\t\t    *");
+        System.out.println(";*16 bit - intOne = " +intOne +"   \t        *");
+        System.out.println(";*16 bit - longOne = " +longOne +" \t\t        *");
+        System.out.println("              real types                    *");
+        System.out.println(";*32 bit - floatOne = " +floatOne +"\t\t\t        *");
+        System.out.println(";*64 bit - doubleOne = " +doubleOne +" \t\t\t    *");
+        System.out.println(";*          other integer types\t            *");
+        System.out.println(";*16 bit - charOne = " +charOne +"\t\t\t\t        *");
+        System.out.println("*             boolean = True                *");
+        System.out.println("*String = Hello World                       *");
+
+
+        System.out.println(";////////////////////////////////////////////");
+    }
+}
